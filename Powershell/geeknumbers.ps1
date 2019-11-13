f134a9c583261746380ea03fd7087cf3d2911595
vampire1233 <55846428+vampire1233@users.noreply.github.com>
2019-11-13 17:08:00 -0500

    D

---
+++ Powershell/geeknumbers.ps1
@@ -0,0 +1,8 @@
+﻿foreach ($c in (1..8)) 
+{new-object -typename psobject -property @{
+   PlaceCount=$c;
+   MaxValueInBinary=[math]::pow(2,$c);
+   MaxValueInOctal=[math]::pow(8,$c);
+   MaxValueInHex=[math]::pow(16,$c)
+  }
+ }
